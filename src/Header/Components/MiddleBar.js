1c0d92d45dda55f7285256200d6eadb1be9a6719
Emma Vaughan <emma.vaughan91@hotmail.com>
2021-07-22 11:08:54 +0100

    Amended some styling

--- src/Header/Components/MiddleBar.js
+++ src/Header/Components/MiddleBar.js
@@ -2,17 +2,17 @@ function MiddleBar() {
   return (
     <div className="middleBar">
       <div className="IP">
-        <h3>IP Address</h3>
+        <h3>IP ADDRESS</h3>
         <p>IP Address</p>
       </div>
       <hr width="1" size="75" />
       <div className="location">
-        <h3>Location</h3>
+        <h3>LOCATION</h3>
         <p>Location</p>
       </div>
       <hr width="1" size="75" />
       <div className="timezone">
-        <h3>Timezone</h3>
+        <h3>TIMEZONE</h3>
         <p>Timezone</p>
       </div>
       <hr width="1" size="75" />
