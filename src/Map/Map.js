cf2f9553a62a42851e262e7dfab726ff96402ee2
Emma Vaughan <emma.vaughan91@hotmail.com>
2021-07-27 16:20:08 +0100

    Amended the map API to use react-lealet
    Remove the useEffect and implemented the map using react-leaflet instead.

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,25 +1,23 @@
-import { useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function Map() {
-  useEffect(() => {
-    var mymap = window.L.map("mapid").setView([51.505, -0.09], 13);
-
-    window.L.tileLayer(
-      "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/1/1/0?access_token=pk.eyJ1IjoiZW1tYW1hcGJveCIsImEiOiJja3JmMGloYnMwdGVnMnZvZXI5NWJ5d24yIn0.jySqpbxZgDEQfiq79ADgaQ",
-      {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: "mapbox/streets-v11",
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken:
-          "pk.eyJ1IjoiZW1tYW1hcGJveCIsImEiOiJja3JmMGloYnMwdGVnMnZvZXI5NWJ5d24yIn0.jySqpbxZgDEQfiq79ADgaQ",
-      }
-    ).addTo(mymap);
-  }, []);
-
-  return <div id="mapid"></div>;
+  return (
+    <MapContainer
+      center={[37.7749, -122.4194]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[37.7749, -122.4194]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
 
 export default Map;
