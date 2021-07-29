b070592d3426a78b8ed91d88b39451caf482251f
Emma Vaughan <emma.vaughan91@hotmail.com>
2021-07-29 13:31:01 +0100

    Linked the search bar to the IP and map API's

--- src/Header/Components/Search.js
+++ src/Header/Components/Search.js
@@ -1,9 +1,12 @@
 import { api } from "../../API";
+import { useState } from "react";
 
 function Search({ setData }) {
+  const [input, setInput] = useState("");
+
   function handleClick(e) {
     e.preventDefault();
-    fetch("https://geo.ipify.org/api/v1?apiKey=" + api + "&ipAddress=")
+    fetch("https://geo.ipify.org/api/v1?apiKey=" + api + "&ipAddress=" + input)
       .then((response) => response.json())
       .then((response) => {
         setData(response);
@@ -16,6 +19,8 @@ function Search({ setData }) {
         type="text"
         id="searchBar"
         placeholder="Search for any IP address or domain"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
       />
       <button onClick={handleClick} className="button">
         >
